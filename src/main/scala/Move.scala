1ca65e3a29f92e113ef56f28b461a53fc286fe9f
CosmicRay11 <george.w11@hotmail.co.uk>
2020-05-15 20:57:49 +0100

    Updated the move verification procedure. Doesn't include checking for castling or check yet

--- src/main/scala/Move.scala
+++ src/main/scala/Move.scala
@@ -2,7 +2,16 @@ class Move(init: (Int, Int), fin: (Int, Int), board: Board) {
   def isValid: Boolean = {
     val p1 = board(init._1)(init._2)
     if (p1.isDefined) {
-      true
+      val p2 = board(fin._1)(fin._2)
+      if (!p2.isDefined || p2.get.isWhite != p1.get.isWhite) {
+        val inCheckThen = true // mention castling somewhere here
+        if (inCheckThen) {
+          val inCheckNow = false
+          if (!inCheckNow) {
+            return true
+          }
+        }
+      }
     }
     false
   }
